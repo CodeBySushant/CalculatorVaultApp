b361c2e903908d4b31c0c98f9d245d618a630b1e
Sushant Sharma <itsofficialsushant@gmail.com>
2026-07-03 18:53:36 +0545

    fix(android): AGP 8.13 + Gradle 8.14.3, remove build-dir relocation, low-RAM JVM config

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -5,11 +5,7 @@
     }
 }
 
-// --- Build output redirect (required so `flutter run` can find the APK). ---
-rootProject.layout.buildDirectory.set(file("${rootProject.projectDir}/../build"))
-
 subprojects {
-    layout.buildDirectory.set(file("${rootProject.projectDir}/../build/${project.name}"))
     project.evaluationDependsOn(":app")
 }
 
